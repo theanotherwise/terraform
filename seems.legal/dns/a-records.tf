30b142c6922ddc0106d9f7637f728c1b0509b06d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-07-19 21:19:07 +0200

    added full greek alphabet

--- seems.legal/dns/a-records.tf
+++ seems.legal/dns/a-records.tf
@@ -50,45 +50,6 @@ resource "google_dns_record_set" "a-gamma-seems-legal" {
     google_dns_managed_zone.public-seems-legal]
 }
 
-resource "google_dns_record_set" "a-kappa-seems-legal" {
-  name = "kappa.seems.legal."
-  managed_zone = google_dns_managed_zone.public-seems-legal.name
-  type = "A"
-  ttl = 300
-
-  rrdatas = [
-    "8.8.8.8"]
-
-  depends_on = [
-    google_dns_managed_zone.public-seems-legal]
-}
-
-resource "google_dns_record_set" "a-omega-seems-legal" {
-  name = "omega.seems.legal."
-  managed_zone = google_dns_managed_zone.public-seems-legal.name
-  type = "A"
-  ttl = 300
-
-  rrdatas = [
-    "8.8.8.8"]
-
-  depends_on = [
-    google_dns_managed_zone.public-seems-legal]
-}
-
-resource "google_dns_record_set" "a-zeta-seems-legal" {
-  name = "zeta.seems.legal."
-  managed_zone = google_dns_managed_zone.public-seems-legal.name
-  type = "A"
-  ttl = 300
-
-  rrdatas = [
-    "8.8.8.8"]
-
-  depends_on = [
-    google_dns_managed_zone.public-seems-legal]
-}
-
 resource "google_dns_record_set" "a-delta-seems-legal" {
   name = "delta.seems.legal."
   managed_zone = google_dns_managed_zone.public-seems-legal.name
@@ -102,9 +63,255 @@ resource "google_dns_record_set" "a-delta-seems-legal" {
     google_dns_managed_zone.public-seems-legal]
 }
 
-
 resource "google_dns_record_set" "a-epsilon-seems-legal" {
   name = "epsilon.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-dzeta-seems-legal" {
+  name = "dzeta.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-eta-seems-legal" {
+  name = "eta.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-theta-seems-legal" {
+  name = "theta.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-iota-seems-legal" {
+  name = "iota.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-kappa-seems-legal" {
+  name = "kappa.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-lambda-seems-legal" {
+  name = "lambda.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-my-seems-legal" {
+  name = "my.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-ny-seems-legal" {
+  name = "ny.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-xy-seems-legal" {
+  name = "xy.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-omcicron-seems-legal" {
+  name = "omnicron.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-pi-seems-legal" {
+  name = "pi.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-rho-seems-legal" {
+  name = "rho.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-sigma-seems-legal" {
+  name = "sigma.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-tau-seems-legal" {
+  name = "tau.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-ypsilon-seems-legal" {
+  name = "ypsilon.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-phi-seems-legal" {
+  name = "phi.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-chi-seems-legal" {
+  name = "chi.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-psi-seems-legal" {
+  name = "psi.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "8.8.8.8"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-omega-seems-legal" {
+  name = "omega.seems.legal."
   managed_zone = google_dns_managed_zone.public-seems-legal.name
   type = "A"
   ttl = 300
