d33dddfdd18b40a3d59859500f9bbb1f961a573d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-07-23 20:32:23 +0200

    #

--- seems.legal/dns/g-suite.tf
+++ seems.legal/dns/g-suite.tf
@@ -19,7 +19,7 @@ resource "google_dns_record_set" "txt-seems-legal" {
 
   rrdatas = [
     "\"v=spf1 include:_spf.google.com ~all\"",
-    "\"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi1jos0mec95dXckogWgIVzYAKbn9Zi9ocSj2pJGszEsgkq//dmbjRO7gddN3hK55IXQnwGMx+0/4mozWenTJEubIqCcYQbD39T+P6IQJ9uhEhzCccBxRSetHLWb+eGcOX6Zwgc2kOKTCeUnsTX8DZmEoiGPYHz7snKaR5J9bKGCk5hMvg4TAhjberhjyg9M9V\" \"9V4zriUycFMd4zoLf1bR0dGHIhdW4I4a/aeLVx8rm9G4MXtOrN3mDIyoJPMJxJ/DzDax9lqkUVOw8WjcSaj/DWd2dGhZKWep3ZoS3Zp+vR/3UUE0UUTzgquAM5iEFsp6a43ET2ZQxo0HI+qUwSQFwIDAQAB\""]
+    "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCkE0sIXS3aBQN5HsnnJIS8MRaUMmUaXMVO7e6ZYprMP2cd3KGxAGSeWN1IjuWjYzHH8P8WX8OGDczmc1AECdxe/0meix0di/Cr093eQdG7HIKe4NFw94ENxNrK4GrPAZE6DEZ71KCGXDX4IBWYK8JQv/lOx3Jlsl8iWw3cchrfqwIDAQAB"]
 
   depends_on = [
     google_dns_managed_zone.public-seems-legal]
--- seems.legal/dns/seems.legal.tf
+++ seems.legal/dns/seems.legal.tf
@@ -5,19 +5,19 @@ resource "google_dns_managed_zone" "public-seems-legal" {
   visibility = "public"
 }
 
-resource "google_dns_record_set" "ns-seems-legal" {
-  name = "seems.legal."
-  managed_zone = google_dns_managed_zone.public-seems-legal.name
-  type = "NS"
-  ttl = 300
-
-  rrdatas = [
-    "ns-cloud-e1.googledomains.com.",
-    "ns-cloud-e2.googledomains.com.",
-    "ns-cloud-e3.googledomains.com.",
-    "ns-cloud-e4.googledomains.com."]
-
-  depends_on = [
-    google_dns_managed_zone.public-seems-legal]
-}
+#resource "google_dns_record_set" "ns-seems-legal" {
+#  name = "seems.legal."
+#  managed_zone = google_dns_managed_zone.public-seems-legal.name
+#  type = "NS"
+#  ttl = 300
+#
+#  rrdatas = [
+#    "ns-cloud-e1.googledomains.com.",
+#    "ns-cloud-e2.googledomains.com.",
+#    "ns-cloud-e3.googledomains.com.",
+#    "ns-cloud-e4.googledomains.com."]
+#
+#  depends_on = [
+#    google_dns_managed_zone.public-seems-legal]
+#}
 
