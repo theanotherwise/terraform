fba353250c9a895a3871cf4b4ec191ba9c7171ab
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-08-30 06:16:27 +0200

    #

--- seems.legal/dns-external/seems.cloud/mx-records.tf
+++ seems.legal/dns-external/seems.cloud/mx-records.tf
@@ -9,12 +9,7 @@ resource "google_dns_record_set" "mx-seems-cloud" {
     "10 alt1.gmr-smtp-in.l.google.com.",
     "20 alt2.gmr-smtp-in.l.google.com.",
     "30 alt3.gmr-smtp-in.l.google.com.",
-    "40 alt4.gmr-smtp-in.l.google.com.",
-    "1 aspmx.l.google.com.",
-    "5 alt1.aspmx.l.google.com.",
-    "5 alt2.aspmx.l.google.com.",
-    "10 alt3.aspmx.l.google.com.",
-    "10 alt4.aspmx.l.google.com."]
+    "40 alt4.gmr-smtp-in.l.google.com."]
 
   depends_on = [
     google_dns_managed_zone.public-seems-cloud]
